ff72b6a30a9c7f121ed97fe0a7b9cc013345cdd9
Student User <student@workstation.lab.example.com>
2022-03-09 09:17:49 -0500

    Added phpinfo.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n";
+print "<H1>Hello, World! php version is: " . PHP_VERSION . "</H1>\n";
+print "<P>A change is a coming!i</P>\n";
+phpinfo();
 ?>
