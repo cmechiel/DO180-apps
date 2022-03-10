f19b76b51c25f68d2c0faacd37f6683defc72046
Student User <student@workstation.lab.example.com>
2022-03-10 03:50:57 -0500

    back to the older version

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,3 @@
 <?php
-print "<H1>Hello, World! php version is: " . PHP_VERSION . "</H1>\n";
-print "<P>A change is a coming!i</P>\n";
-phpinfo();
+print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
